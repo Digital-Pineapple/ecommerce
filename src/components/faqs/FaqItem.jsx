db706239493b406c6fbe924a95c85d8cb7439728
Irving Rodriguez <irving@digital-pineapple.com.mx>
2023-10-13 12:55:53 -0600

    show info faqs helps

--- src/components/faqs/FaqItem.jsx
+++ src/components/faqs/FaqItem.jsx
@@ -1,20 +1,21 @@
-
-import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
+import HelpOutlineIcon from "@mui/icons-material/HelpOutline";
 
 const FaqItem = ({ faq }) => {
+  return (
+    <div>
+      <div className="my-5 flex items-center">
+        <HelpOutlineIcon className="text-[25px] text-[#000]" />
+        <p className="font-bold text-xl px-2" id={`${faq._id}`}>
+          {faq.question}
+        </p>
+      </div>
+      <div
+        className="pl-10 pb-10"
+        dangerouslySetInnerHTML={{ __html: faq.answer }}
+      ></div>
+      <hr />
+    </div>
+  );
+};
 
-    return (
-        <div>
-            <div className="my-5 flex items-center">
-                <HelpOutlineIcon 
-                    className="text-[25px] text-[#000]"
-                />
-                <p className="font-bold text-xl px-2" id={`${faq._id}`}>{faq.question}</p>
-            </div>
-            <p className="pl-10 pb-10">{faq.answer}</p>
-            <hr />
-        </div>
-    )
-}
-
-export default FaqItem
+export default FaqItem;
--- src/components/faqs/FaqsList.jsx
+++ src/components/faqs/FaqsList.jsx
@@ -1,19 +1,13 @@
-import FaqItem from "./FaqItem"
-
+import FaqItem from "./FaqItem";
 
 const FaqsList = ({ faqs }) => {
-    return (
-        <div className="lg:col-span-2">
-            {
-                faqs.map(faq => (
-                    <FaqItem
-                        faq={faq}
-                        key={faq._id}
-                    />
-                ))
-            }
-        </div>
-    )
-}
+  return (
+    <div className="lg:col-span-2">
+      {faqs.map((faq) => (
+        <FaqItem faq={faq} key={faq._id} />
+      ))}
+    </div>
+  );
+};
 
-export default FaqsList
+export default FaqsList;
