732d37bddd9e809dd7b6a8d3b17f80f36cee03c8
Irving Rodriguez <irving@digital-pineapple.com.mx>
2023-10-21 11:33:32 -0600

    add phone number contact

--- pages/contacto/index.jsx
+++ pages/contacto/index.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { startLoadAdministrableLogo } from "../../src/actions/administrableActions";
-import Layout from "../../src/components/Layouts"
+import Layout from "../../src/components/Layouts";
 import { BannerImage, ContactInfo } from "../../src/components/ui";
 import { wrapper } from "../../src/store";
 
@@ -8,60 +8,62 @@ import { useDispatch, useSelector } from "react-redux";
 import { startLoadFaqsCategories } from "../../src/actions/faqsActions";
 
 import { useFormik } from "formik";
-import * as Yup from 'yup';
+import * as Yup from "yup";
 import { startSendMessage } from "../../src/actions/contactActions";
 import LoadingScreen from "../../src/components/LoadingScreen";
 import { errorNotify, successNotify } from "../../src/helpers/helpers";
 
-import PersonIcon from '@mui/icons-material/Person';
-import EmailIcon from '@mui/icons-material/Email';
-import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
+import PersonIcon from "@mui/icons-material/Person";
+import EmailIcon from "@mui/icons-material/Email";
+import LocalPhoneIcon from "@mui/icons-material/LocalPhone";
 
-import LocationOnIcon from '@mui/icons-material/LocationOn';
+import LocationOnIcon from "@mui/icons-material/LocationOn";
 import { startLoadCurrencies } from "../../src/actions/countryAcctions";
 
 const ContactUs = () => {
-  
   const { logo } = useSelector((state) => state.administrable);
   const { categories } = useSelector((state) => state.faqs);
 
   const [loading, setLoading] = useState(false);
 
   const handelSendMessage = async (formData, resetForm) => {
-
     setLoading(true);
 
     const { hasError, message } = await startSendMessage(formData);
 
     if (hasError) {
-      errorNotify(message)
+      errorNotify(message);
       return;
     }
     successNotify(message);
-    resetForm({ values: initialValues })
+    resetForm({ values: initialValues });
     setLoading(false);
-  }
+  };
 
   const initialValues = {
-    name: '',
-    email: '',
-    phone: '',
-    message: '',
-  }
+    name: "",
+    email: "",
+    phone: "",
+    message: "",
+  };
 
   const validationSchema = {
-    name: Yup.string().min(8, "El nombre debe contener al menos 8 caracteres").required("El nombre es requerido"),
+    name: Yup.string()
+      .min(8, "El nombre debe contener al menos 8 caracteres")
+      .required("El nombre es requerido"),
     email: Yup.string().email(true).required("El correo es requerido"),
     phone: Yup.string().required("La telefono es requerida"),
-    message: Yup.string().min(8, "El mensaje debe contener al menos 8 caracteres").required("El mensaje es requerido"),
-  }
+    message: Yup.string()
+      .min(8, "El mensaje debe contener al menos 8 caracteres")
+      .required("El mensaje es requerido"),
+  };
 
   const formik = useFormik({
     initialValues,
     validationSchema: Yup.object(validationSchema),
     onSubmit: (formData, { resetForm }) => {
       handelSendMessage(formData, resetForm);
-    }
+    },
   });
 
   const origin = typeof window === "undefined" ? "" : window.location.href;
@@ -80,21 +82,21 @@ const ContactUs = () => {
       canonical={origin}
     >
       {loading && <LoadingScreen />}
-      <BannerImage
-        title="Contáctanos"
-        banner="bg-banner5"
-      />
+      <BannerImage title="Contáctanos" banner="bg-banner5" />
       <section className="bg-luz min-h-screen">
         <div>
           <div className=" max-w-[1380px] mx-auto w-full my-20">
             <div className="flex flex-wrap">
               <div className="border-solid border-[2px] border-[#e6e6e6] w-full lg:w-1/2 xl:w-1/2 p-16 flex justify-center flex-wrap items-center">
                 <form className="w-full" onSubmit={formik.handleSubmit}>
-                  <h4 className="font-Poppins text-[24px] leading-[1.5] text-[#333] text-center pb-[30px]">Envianos un mensaje</h4>
+                  <h4 className="font-Poppins text-[24px] leading-[1.5] text-[#333] text-center pb-[30px]">
+                    Envianos un mensaje
+                  </h4>
                   <div className="mb-6">
                     <div className="border-[1px] border-solid border-[#e6e6e6] rounded-sm flex items-center">
-                      <PersonIcon className="text-[25px] text-[#888] w-[20%]"  />
-                      <input type="text"
+                      <PersonIcon className="text-[25px] text-[#888] w-[20%]" />
+                      <input
+                        type="text"
                         name="name"
                         placeholder="Ingresa tu nombre"
                         onChange={formik.handleChange}
@@ -102,7 +104,9 @@ const ContactUs = () => {
                       />
                     </div>
                     {formik.touched.name && formik.errors.name ? (
-                      <span className="text-red-500 text-sm">{formik.errors.name}</span>
+                      <span className="text-red-500 text-sm">
+                        {formik.errors.name}
+                      </span>
                     ) : null}
                   </div>
                   <div className="mb-6">
@@ -117,12 +121,14 @@ const ContactUs = () => {
                       />
                     </div>
                     {formik.touched.email && formik.errors.email ? (
-                      <span className="text-red-500 text-sm">{formik.errors.email}</span>
+                      <span className="text-red-500 text-sm">
+                        {formik.errors.email}
+                      </span>
                     ) : null}
                   </div>
                   <div className="mb-6">
                     <div className="border-[1px] border-solid border-[#e6e6e6] rounded-sm flex items-center">
-                      <LocalPhoneIcon className="text-[25px] text-[#888] w-[20%]"/>
+                      <LocalPhoneIcon className="text-[25px] text-[#888] w-[20%]" />
                       <input
                         type="text"
                         name="phone"
@@ -132,7 +138,9 @@ const ContactUs = () => {
                       />
                     </div>
                     {formik.touched.phone && formik.errors.phone ? (
-                      <span className="text-red-500 text-sm">{formik.errors.phone}</span>
+                      <span className="text-red-500 text-sm">
+                        {formik.errors.phone}
+                      </span>
                     ) : null}
                   </div>
                   <div className="mb-6">
@@ -145,10 +153,13 @@ const ContactUs = () => {
                       ></textarea>
                     </div>
                     {formik.touched.message && formik.errors.message ? (
-                      <span className="text-red-500 text-sm">{formik.errors.message}</span>
+                      <span className="text-red-500 text-sm">
+                        {formik.errors.message}
+                      </span>
                     ) : null}
                   </div>
-                  <button className="bg-[#222] w-full text-white py-4 uppercase hover:bg-[#333] border-2 border-[#222] transition-all duration-700 ease-in-out"
+                  <button
+                    className="bg-[#222] w-full text-white py-4 uppercase hover:bg-[#333] border-2 border-[#222] transition-all duration-700 ease-in-out"
                     type="submit"
                   >
                     Enviar
@@ -156,7 +167,6 @@ const ContactUs = () => {
                 </form>
               </div>
               <div className="border-solid border-[2px] border-[#e6e6e6] w-full lg:w-1/2 xl:w-1/2 p-16 flex justify-center flex-wrap items-center">
-
                 <ContactInfo
                   icon={<LocationOnIcon />}
                   title="Dirección"
@@ -168,6 +178,7 @@ const ContactUs = () => {
                   title="Hablemos"
                   text="+52 (722 781 5291)"
                   phonetwo="+52 (729 143 9285)"
+                  phonethree="+52 (729 322 2418)"
                 />
 
                 <ContactInfo
@@ -180,14 +191,19 @@ const ContactUs = () => {
           </div>
         </div>
         <div className="w-full">
-          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3766.14544889983!2d-99.66265068578412!3d19.276040050664797!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85cd89a337ab3863%3A0xe5209d75006ddf53!2sDr.%20Andr%C3%A9s%20Benavides%20304%2C%20Residencial%20Col%C3%B3n%20y%20Col%20Cipr%C3%A9s%2C%2050120%20Toluca%20de%20Lerdo%2C%20M%C3%A9x.!5e0!3m2!1ses-419!2smx!4v1650566416705!5m2!1ses-419!2smx" width="600" height="450" loading="lazy" referrerPolicy="no-referrer-when-downgrade"
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3766.14544889983!2d-99.66265068578412!3d19.276040050664797!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85cd89a337ab3863%3A0xe5209d75006ddf53!2sDr.%20Andr%C3%A9s%20Benavides%20304%2C%20Residencial%20Col%C3%B3n%20y%20Col%20Cipr%C3%A9s%2C%2050120%20Toluca%20de%20Lerdo%2C%20M%C3%A9x.!5e0!3m2!1ses-419!2smx!4v1650566416705!5m2!1ses-419!2smx"
+            width="600"
+            height="450"
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
             className="border-0 w-full"
           ></iframe>
         </div>
       </section>
     </Layout>
-  )
-}
+  );
+};
 
 export const getStaticProps = wrapper.getStaticProps((store) => async () => {
   await store.dispatch(startLoadAdministrableLogo());
@@ -195,9 +211,8 @@ export const getStaticProps = wrapper.getStaticProps((store) => async () => {
   await store.dispatch(startLoadCurrencies());
 
   return {
-    revalidate: 300
-  }
+    revalidate: 300,
+  };
 });
 
-
-export default ContactUs; 
+export default ContactUs;
--- src/components/ui/contactInfo.jsx
+++ src/components/ui/contactInfo.jsx
@@ -1,24 +1,25 @@
-
-
-export const ContactInfo = ({icon , title , text, phonetwo}) =>{
-    return (
-        <div className="w-full flex flex-wrap pb-[42px] items-center">
-                         <span className="w-[60px] text-[#222] text-center text-lg">
-                         <span
-                               value={{className:"text-[25px] text-[#888] w-[35%]"}}
-                            >
-                           {icon}
-                         </span>
-                         </span>
-                         <div className="w-[calc(100%_-_60px)] pt-[2px]">
-                           <span className="font-Poppins text-lg leading-[1.2] text-[#333]">{title}</span>
-                           <p className="max-w-[245px] font-Poppins text-[15px] leading-[1.666] text-[#888]">
-                             {text}
-                           </p>
-                           <p className="max-w-[245px] font-Poppins text-[15px] leading-[1.666] text-[#888]">
-                             {phonetwo}
-                           </p>
-                         </div>
-        </div>
-    )
-}
+export const ContactInfo = ({ icon, title, text, phonetwo }) => {
+  return (
+    <div className="w-full flex flex-wrap pb-[42px] items-center">
+      <span className="w-[60px] text-[#222] text-center text-lg">
+        <span value={{ className: "text-[25px] text-[#888] w-[35%]" }}>
+          {icon}
+        </span>
+      </span>
+      <div className="w-[calc(100%_-_60px)] pt-[2px]">
+        <span className="font-Poppins text-lg leading-[1.2] text-[#333]">
+          {title}
+        </span>
+        <p className="max-w-[245px] font-Poppins text-[15px] leading-[1.666] text-[#888]">
+          {text}
+        </p>
+        <p className="max-w-[245px] font-Poppins text-[15px] leading-[1.666] text-[#888]">
+          {phonetwo}
+        </p>
+        <p className="max-w-[245px] font-Poppins text-[15px] leading-[1.666] text-[#888]">
+          {phonethree}
+        </p>
+      </div>
+    </div>
+  );
+};
